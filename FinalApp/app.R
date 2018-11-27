c8c99ce16facae49b16053988660d5ee9759c77b
Sonya Kalara <sonyakalara@Sonyas-MacBook-Air.local>
2018-11-27 13:02:41 -0500

    County lines added to leaflet.

--- FinalApp/app.R
+++ FinalApp/app.R
@@ -7,44 +7,40 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(tidyverse)
+
 library(urbnmapr)
+library(haven)
+library(fs)
+library(readxl)
+library(lubridate)
+library(kableExtra)
+library(janitor)
+library(leaflet)
+library(sp)
+library(rgdal)
+library(raster)
+library(tidyverse)
+library(moderndive)
+library(tigris)
+
+newYork <- counties("New York", cb = TRUE)
+
+hateCrimes <- read_csv("Hate_Crimes_by_County_and_Bias_Type__Beginning_2010.csv")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+        leafletOutput("mymap", height = "350px")
       )
-   )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$mymap <- renderLeaflet({
+    leaflet(tibbleShapes) %>%
+      addProviderTiles(provider = "CartoDB") %>% 
+      addPolygons()
+  })
 }
 
 # Run the application 
